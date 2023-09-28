0dfd45eac032a1c5b51d03b1c95b11aba75ce1d6
Ted <ted@wu.engineer>
2023-09-29 03:21:07 +0800

    Finish Assignment 6

--- Lab/Assignment6/imageComposition_Skeleton.py
+++ Lab/Assignment6/imageComposition_Skeleton.py
@@ -4,6 +4,37 @@ import matplotlib.pyplot as plt
 
 
 def image_composition(filename1, filename2):
+    # Load the foreground and background images
+    foreground = imageio.imread(filename1)
+    background = imageio.imread(filename2)
+
+    # Iterate over the pixels
+    for i in range(foreground.shape[0]):
+        for j in range(foreground.shape[1]):
+            r, g, b = foreground[i, j]
+            if g > r and g > b and g > 110:  # Check for green criteria
+                foreground[i, j] = background[i, j]
+
+    # Save the resultant image
+    imageio.imwrite('aveNUgerS.jpg', foreground)
+
+    # Display the original images
+    plt.figure(figsize=(10, 5))
+
+    plt.subplot(2, 2, 1)
+    plt.imshow(imageio.imread(filename1))
+    plt.axis('off')
+
+    plt.subplot(2, 2, 2)
+    plt.imshow(imageio.imread(filename2))
+    plt.axis('off')
+
+    plt.subplot(2, 1, 2)
+    plt.imshow(foreground)
+    plt.axis('off')
+
+    plt.tight_layout()
+    plt.show()
     return
 
 #Uncomment the following for your own testing
@@ -11,6 +42,6 @@ def image_composition(filename1, filename2):
 #image_composition('avengers green.jpg','background.jpg')
 
 
-#image_composition('avengers green triangle.jpg','background.jpg')
+image_composition('avengers green triangle.jpg','background.jpg')
 
 
--- Lab/Assignment6/pizzaMap_skeleton.py
+++ Lab/Assignment6/pizzaMap_skeleton.py
@@ -1,4 +1,3 @@
-
 def createZeroMatrix(n,m):
     return [[0 for i in range(m)] for j in range(n)]
 
@@ -10,11 +9,55 @@ def mTightPrint(m):
         print(line)
         
 def PDMap(r,c,sites):
-    return
+    # Create a empty map
+    pizza_map = createZeroMatrix(r, c)
+    row_num = -1
+    # For each element
+    for row in pizza_map:
+        row_num += 1
+        for col in range(c):
+            distance = []
+            # Calculate each store distance
+            for cor in sites:
+                distance.append(sqrt(abs(row_num-cor[0]) ** 2 + abs(col-cor[1]) ** 2))
+            # Find min distance and min store count
+            min_dis = min(distance)
+            count_min = sum(1 for x in distance if x == min_dis)
+            if count_min == 1:
+                pizza_map[row_num][col] = distance.index(min_dis)
+            else:
+                # Return x if multiple stores
+                pizza_map[row_num][col] = 'X'
+    return pizza_map
+
+
+def sqrt(x, tolerance=1e-10, max_iterations=1000):
+    """
+    Newton-Raphson Method of squared root
+    :param x: numbers need to sqrt
+    :param tolerance: threshold value
+    :param max_iterations: maximum iterations
+    :return: squared root of x
+    """
+
+    if x < 0:
+        raise ValueError("Cannot compute the square root of a negative number.")
+
+    if x == 0 or x == 1:
+        return x
+
+    guess = x / 2.0
+    for _ in range(max_iterations):
+        next_guess = (guess + x / guess) / 2.0
+        if abs(next_guess - guess) < tolerance:
+            return next_guess
+        guess = next_guess
+
+    return guess
+
 
 #mTightPrint(PDMap(50,80,[[20,10], [30,30],[40,20],[45,55],[10,55],[35,70],[35,60]]))
 mTightPrint(PDMap(10,10,[[2,3],[4,9],[7,2]]))
-
-mTightPrint(PDMap(60,70,[[10,20],[30,20],[40,50]]))
-ex = PDMap(60,70,[[10,20],[30,20],[40,50]])
+# PDMap(60,70,[[10,20],[30,20],[40,50]])
+#ex = PDMap(60,70,[[10,20],[30,20],[40,50]])
     
